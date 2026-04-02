fe2c9e8f5b3c358af34ce98a7901cbe9a180310b
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-04-02 12:17:08 +0200

    add schema creation from expressions

--- src/pql/_schema.py
+++ src/pql/_schema.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Self
 
 import pyochain as pc
+from sqlglot import exp
+from sqlglot.optimizer.annotate_types import (
+    annotate_types,  # pyright: ignore[reportUnknownVariableType]
+)
+from sqlglot.optimizer.qualify import (
+    qualify,  # pyright: ignore[reportUnknownVariableType]
+)
 from sqlglot.schema import MappingSchema
 
 from ._datatypes import DataType
@@ -18,6 +26,41 @@ class Schema(pc.Dict[str, DataType]):
     def from_frame(cls, frame: DuckDBPyRelation) -> Self:
         dtypes = pc.Iter(frame.dtypes).map(DataType.from_duckdb)
         return pc.Iter(frame.columns).zip(dtypes, strict=True).collect(cls)
+
+    @classmethod
+    def from_exprs(
+        cls,
+        input_schema: Schema,
+        exprs: Iterable[tuple[str, exp.Expr]],
+        *,
+        table_name: str = "rel",
+    ) -> Self:
+        mapping_schema = input_schema.to_mapping_schema(table_name)
+
+        def _infer_dtype(name: str, expression: exp.Expr) -> tuple[str, DataType]:
+            query = exp.select(
+                exp.Alias(this=expression.copy(), alias=exp.to_identifier(name))
+            ).from_(table_name)
+            qualified = qualify(
+                query,
+                dialect="duckdb",
+                schema=mapping_schema,
+                validate_qualify_columns=False,
+            )
+            annotated = annotate_types(
+                qualified,
+                schema=mapping_schema,
+                dialect="duckdb",
+            )
+            dtype = (
+                pc
+                .Option(annotated.type)
+                .map(DataType.from_sql)
+                .expect("Failed to infer data type")
+            )
+            return (name, dtype)
+
+        return pc.Iter(exprs).map_star(_infer_dtype).collect(cls)
 
     def to_mapping_schema(self, table_name: str) -> MappingSchema:
         type_map = (
