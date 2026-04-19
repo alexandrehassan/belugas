13d094c9b27e14fc26a4583650e247fb114854b2
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-04-19 14:46:49 +0200

    refactor (tests): use constants instead of functions

--- tests/test_dt_namespace.py
+++ tests/test_dt_namespace.py
@@ -1,20 +1,14 @@
 import polars as pl
 import pytest
-from polars.expr.datetime import ExprDateTimeNameSpace
 
 import pql
 import pql.sql.typing as t
-from pql._namespaces import ExprDateTimeNameSpace as PqlExprDateTimeNameSpace
 
 from ._utils import assert_eq, on_simple_fn
 
-
-def _pql_dt() -> PqlExprDateTimeNameSpace:
-    return pql.col("dt").dt
-
-
-def _pl_dt() -> ExprDateTimeNameSpace:
-    return pl.col("dt").dt
+dt = "dt"
+pql_dt = pql.col(dt).dt
+pl_dt = pl.col(dt).dt
 
 
 _SIMPLE_FNS = {
@@ -43,31 +37,31 @@ _SIMPLE_FNS = {
 
 @pytest.mark.parametrize("fn", sorted(_SIMPLE_FNS))
 def test_simple_fns(fn: str) -> None:
-    on_simple_fn(_pql_dt(), _pl_dt(), fn)
+    on_simple_fn(pql_dt, pl_dt, fn)
 
 
 @pytest.mark.parametrize("unit", t.EpochTimeUnit.__args__)
 def test_epoch(unit: t.EpochTimeUnit) -> None:
-    assert_eq(_pql_dt().epoch(unit), _pl_dt().epoch(unit))
+    assert_eq(pql_dt.epoch(unit), pl_dt.epoch(unit))
 
 
 @pytest.mark.parametrize("unit", t.TimeUnit.__args__)
 def test_timestamp(unit: t.TimeUnit) -> None:
-    assert_eq(_pql_dt().timestamp(unit), _pl_dt().timestamp(unit))
+    assert_eq(pql_dt.timestamp(unit), pl_dt.timestamp(unit))
 
 
 @pytest.mark.parametrize("fmt", ["%Y-%m-%d", "%H:%M"])
 def test_date_time_boundaries(fmt: str) -> None:
-    assert_eq(_pql_dt().to_string(fmt), _pl_dt().to_string(fmt))
+    assert_eq(pql_dt.to_string(fmt), pl_dt.to_string(fmt))
 
 
 def test_truncate() -> None:
-    assert_eq(_pql_dt().truncate("month"), _pl_dt().truncate("1mo"))
+    assert_eq(pql_dt.truncate("month"), pl_dt.truncate("1mo"))
 
 
 def test_round() -> None:
-    assert_eq(_pql_dt().round("month"), _pl_dt().round("1mo"))
+    assert_eq(pql_dt.round("month"), pl_dt.round("1mo"))
 
 
 def test_offset_by() -> None:
-    assert_eq(_pql_dt().offset_by("1 day"), _pl_dt().offset_by("1d"))
+    assert_eq(pql_dt.offset_by("1 day"), pl_dt.offset_by("1d"))
